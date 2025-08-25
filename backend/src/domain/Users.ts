372e844e58f64a82c0ba9facec2bf314a1bf4a02
TenjoD <dtenjo@uniempresarial.edu.co>
2025-08-25 15:56:52 -0500

    cambiolenguaje

---
+++ backend/src/domain/Users.ts
@@ -0,0 +1,14 @@
+export interface Usuarios{
+    id: number;
+    nombre: string;
+    apellidos: string;
+    tipo_doc: number;
+    numero_documento: number;
+    direccion: string;
+    telefono: string;
+    departamento: string;
+    ciudad: string;
+    correo: string;
+    contraseña:string;
+    rol: number;
+}
